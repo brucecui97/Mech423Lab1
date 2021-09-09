d0b5791083a33a3ffc74d6f393ac155e286a4a74
bruce <brucec@hotmail.com>
2021-09-09 15:31:39 -0700

    rename class to lab1
    d

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp1
+﻿namespace Lab1
 {
     partial class Form1
     {
@@ -80,7 +80,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(175, 6);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(355, 259);
+            this.pictureBox1.Size = new System.Drawing.Size(372, 259);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
